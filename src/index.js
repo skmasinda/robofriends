a8d5a9e930e5c3462771633b9ab4a950f74c69a9
Samuel Masinda <skmasinda@gmail.com>
2020-06-15 09:46:48 +0200

    robot card

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render( < Hello greeting={'Hello' + ' React Ninja'}/> , document.getElementById('root'));
+ReactDOM.render( 
+	<div>
+		<Card/> 
+		<Card/>
+		<Card/>
+	</div>
+
+	, document.getElementById('root'));
 
 
 serviceWorker.unregister();
